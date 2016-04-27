2d67ca17f9e2da44cbe787b033a165e8ff398527
Aaronduino <letters2aaron@gmail.com>
2016-04-27 16:44:57 -0700

    Fixed and improved setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,19 @@
 
 from distutils.core import setup
 
-setup(name='Distutils',
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
+setup(name='ConvNetPy',
       version='1.0',
       description='Python port of ConvNetJS',
+      long_description=long_description,
+      license='MIT'
       url='https://github.com/Aaronduino/ConvNetPy',
       packages=['convnetpy', 'convnetpy.layers'],
      )
