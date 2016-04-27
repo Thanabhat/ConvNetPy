9cb070a3d51e7ba6dda178610499fbff25c8c190
Aaronduino <letters2aaron@gmail.com>
2016-04-27 16:40:02 -0700

    Formatted for use with setup.py
    I created a basic setup.py folder, and moved files into the folder,
    'convnetpy'

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='Python port of ConvNetJS',
+      url='https://github.com/Aaronduino/ConvNetPy',
+      packages=['convnetpy', 'convnetpy.layers'],
+     )
